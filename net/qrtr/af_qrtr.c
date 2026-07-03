a81f89a52e8df67c85316e92f0820cca8768690c
testtamil154-arch <testtamil154@gmail.com>
2026-07-03 23:44:37 +0530

    Update af_qrtr.c

--- net/qrtr/af_qrtr.c
+++ net/qrtr/af_qrtr.c
@@ -812,7 +812,7 @@ static int qrtr_node_enqueue(struct qrtr_node *node, struct sk_buff *skb,
 		rc = skb_put_padto(skb, ALIGN(len, 4) + sizeof(*hdr));
 
 	if (rc) {
-		pr_err("%s: failed to pad size %lu to %lu rc:%d\n", __func__,
+		pr_err("%s: failed to pad size %u to %u rc:%d\n", __func__,
 		       skb->len, ALIGN(skb->len, 4), rc);
 	}
 
